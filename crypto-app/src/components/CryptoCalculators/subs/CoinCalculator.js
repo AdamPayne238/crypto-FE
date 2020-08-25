29c86628e7d7f49af53702c6c7ecc4f8bd334739
Adam Payne <ajpayne238@gmail.com>
2020-08-24 20:26:07 -0400

    setting up basics for crypto-calculator

--- crypto-app/src/components/CryptoCalculators/subs/CoinCalculator.js
+++ crypto-app/src/components/CryptoCalculators/subs/CoinCalculator.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const CoinCalculator = (props) => {
+
+    const [ profit, setProfit ] = useState(0)
+
     // Coin Calculator
     // If I invest (investment) in (cryptoCoin) today (todaysPrice) and it goes up to (X variable price) 
     // It would be worth (potentialPriceCalculation)
     return(
         <div>
             <h1>CoinCalculator</h1>
+            <p>If I invest 
+                <input type="text" placeholder="Choose your amount" name="Crypto" />
+                in <input type="text" placeholder="Choose your coin" name="Crypto" />
+                today and it goes up to
+                <input type="text" placeholder="USD Amount" name="Crypto" />
+                I will make ${profit}
+            </p>
+            
+            
         </div>
     )
 }
